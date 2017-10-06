2f4eacb5893a5d260c8f985eedf93366d093704d
ABHISHEK SHARMA <ABHISHEK SHARMA>
2017-10-07 00:40:58 +0530

    Initial commit

---
+++ UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.lenovo.androidchatapp;
+
+/**
+ * Created by Lenovo on 06-10-2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
